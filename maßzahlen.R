862f55d8028f6bc8fbcb0364265c246c03390f88
Lisa Maria Lechner <Lisa.Lechner@uibk.ac.at>
2021-12-15 13:55:03 +0100

    update data and create new R skript for simple descriptive metrics

---
+++ maßzahlen.R
@@ -0,0 +1,73 @@
+#######################################
+# Maßzahlen
+#######################################
+
+library(tidyverse)
+library(readxl)
+
+# Einlesen der kontinuierlichen Variable (Quelle: EPI https://epi.yale.edu/epi-results/2020/component/ape)
+emission <- read_excel('./data/epi_pollution_emission.xlsx')
+
+# Einlesen der kategorischen Variable (Quelle: Climate Change Tracker: https://climateactiontracker.org/)
+climatetracker <- read_excel('./data/ratings_climateactiontracker.xlsx')
+
+
+#------
+# Maße der Zentralen Tendenz
+#------
+
+# arithmetisches Mittel
+
+mean(emission$pollution_emission_change)
+
+# Median
+
+median(emission$pollution_emission_change)
+
+# Modus
+modus <- function(V) {
+  uniqV <- unique(V)
+  uniqV[which.max(tabulate(match(V, uniqV)))]
+}
+
+modus(climatetracker$rating)
+
+#-----
+# Maße der Streuung
+#-----
+
+#-----
+# Varianz
+#-----
+
+var(emission$pollution_emission_change)
+
+
+#-----
+# Standardabweichung
+#-----
+
+sqrt(var(emission$pollution_emission_change))
+
+sd(emission$pollution_emission_change)
+
+
+#-----
+# Interquartilsabstand
+#-----
+
+IQR(emission$pollution_emission_change)
+
+
+#----
+# Zusammenfassung
+#----
+
+summary(emission$pollution_emission_change)
+
+
+#-----
+# Häufigkeitstabelle
+#-----
+
+table(climatetracker$rating)
