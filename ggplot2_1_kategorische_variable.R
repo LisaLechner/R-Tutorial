1c848f84f376e89ad955c79136670957076b073d
Lisa Maria Lechner <Lisa.Lechner@uibk.ac.at>
2021-12-15 15:06:56 +0100

    update data and add simple ggplot info scripts

---
+++ ggplot2_1_kategorische_variable.R
@@ -0,0 +1,70 @@
+#######################################
+# ggplot für eine dsikrete Variable
+#######################################
+
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+
+# um die Grafiken einfach zu halten (Stichwort Ink-Data Ratio)
+theme_set(theme_minimal())
+
+# Einlesen der kategorischen Variable (Quelle: Climate Change Tracker: https://climateactiontracker.org/)
+climatetracker <- read_excel('./data/ratings_climateactiontracker.xlsx')
+
+
+#------
+# Dichtefunktion
+#------
+
+
+ggplot(climatetracker, aes(rating)) + geom_bar()+
+  labs(x="INDC Zugeständnisse",
+       y="Häufigkeit",
+       title="Verteilung der INDC Zugeständnisse",
+       subtitle = "Daten stammen vom Climate Change Tracker")
+
+ggplot(climatetracker, aes(rating)) + geom_bar()+
+  labs(x="INDC Zugeständnisse",
+       y="Häufigkeit",
+       title="Verteilung der INDC Zugeständnisse",
+       subtitle = "Daten stammen vom Climate Change Tracker")+
+  theme(axis.text.x = element_text(angle = 90))
+
+
+ggplot(climatetracker, aes(rating)) + geom_bar(width=0.1)+
+  labs(x="INDC Zugeständnisse",
+       y="Häufigkeit",
+       title="Verteilung der INDC Zugeständnisse",
+       subtitle = "Daten stammen vom Climate Change Tracker")+
+  theme(axis.text.x = element_text(angle = 90))
+
+
+ggplot(climatetracker, aes(rating)) + geom_bar(width=0.1)+
+  labs(x="INDC Zugeständnisse",
+       y="Häufigkeit",
+       title="Verteilung der INDC Zugeständnisse",
+       subtitle = "Daten stammen vom Climate Change Tracker")+
+  coord_flip()
+  
+
+
+count <- climatetracker %>% mutate(count=1) %>% 
+  group_by(rating) %>% 
+  dplyr::summarise(count=sum(count))
+
+climatetracker <- left_join(climatetracker,count) %>%
+  arrange(desc(count)) 
+
+climatetracker$rating <- factor(climatetracker$rating,levels=unique(climatetracker$rating))
+
+
+ggplot(climatetracker, aes(rating)) + geom_bar(width=0.1)+
+  labs(x="INDC Zugeständnisse",
+       y="Häufigkeit",
+       title="Verteilung der INDC Zugeständnisse",
+       subtitle = "Daten stammen vom Climate Change Tracker")+
+  coord_flip()
+
+
+
---
+++ ggplot2_1_kontinuierliche_variable.R
@@ -0,0 +1,47 @@
+#######################################
+# ggplot für eine kontinuierliche Variable
+#######################################
+
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+
+# um die Grafiken einfach zu halten (Stichwort Ink-Data Ratio)
+theme_set(theme_minimal())
+
+# Einlesen der kontinuierlichen Variable (Quelle: EPI https://epi.yale.edu/epi-results/2020/component/ape)
+emission <- read_excel('./data/epi_pollution_emission.xlsx')
+
+#------
+# Dichtefunktion
+#------
+
+ggplot(emission, aes(pollution_emission_change)) + geom_density()+
+  labs(x="Schadstoffemissionsveränderung von 2005 auf 2014",
+       y="Häufigkeit",
+       title="Dichtefunktion der Schadstoffemissionsveränderung",
+       subtitle = "Daten stammen von EPI (Yale University)")
+
+
+#------
+# Dotplot
+#------
+
+ggplot(emission, aes(pollution_emission_change)) + geom_dotplot()+
+  labs(x="Schadstoffemissionsveränderung von 2005 auf 2014",
+       y="Häufigkeit",
+       title="Verteilung der Schadstoffemissionsveränderung",
+       subtitle = "Daten stammen von EPI (Yale University)")
+
+
+#------
+# Histogramm
+#------
+
+ggplot(emission, aes(pollution_emission_change)) + geom_histogram()+
+  labs(x="Schadstoffemissionsveränderung von 2005 auf 2014",
+       y="Häufigkeit",
+       title="Verteilung der Schadstoffemissionsveränderung",
+       subtitle = "Daten stammen von EPI (Yale University)")
+
+
